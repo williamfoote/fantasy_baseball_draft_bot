ad97f82b3ca00b60245061120f0aebc301d2e6aa
williamfoote <35303895+williamfoote@users.noreply.github.com>
2023-02-25 12:59:41 -0800

    2/25

--- python/draft_scraper.py
+++ python/draft_scraper.py
@@ -70,13 +70,38 @@ def historical_get_draft_details(league_id, season_id):
 def get_player_info(season_id):
     #url = "https://fantasy.espn.com/apis/v3/games/flb/leagueHistory/{}?view=mDraftDetail&view=mSettings&view=mTeam&view=modular&view=mNav&seasonId={}".format(league_id, season_id)
     url = "https://fantasy.espn.com/apis/v3/games/flb/seasons/{}/players?scoringPeriodId=0&view=players_wl".format(season_id)
+    POSITION_MAP = {
+        0: 'C',
+        1: '1B',
+        2: '2B',
+        3: '3B',
+        4: 'SS',
+        5: 'OF',
+        6: '2B/SS',
+        7: '1B/3B',
+        8: 'LF',
+        9: 'CF',
+        10: 'RF',
+        11: 'DH',
+        12: 'UTIL',
+        13: 'P',
+        14: 'SP',
+        15: 'RP',
+        16: 'BE',
+        17: 'IL',
+        19: 'IF', # 1B/2B/SS/3B
+        # reverse TODO
+        # 18, 21, 22 have appeared but unknown what position they correspond to
+    }
     r = requests.get(url,
                     cookies=espn_cookies,
                     headers=custom_headers)
     player_data = r.json()
     df = pd.DataFrame(player_data)
+    df2 = df.loc[pd.notnull(df['eligibleSlots'])].reset_index(drop = True)
     # get only needed columns for players
-    player_df = df[['defaultPositionId','fullName','id','proTeamId']].copy()
+    player_df = df2[['eligibleSlots', 'defaultPositionId','fullName','id','proTeamId']].copy()
+    player_df.eligibleSlots = [[POSITION_MAP.get(pos, pos) for pos in df2['eligibleSlots'][i] if pos not in [12, 17, 19, 13, 16]] for i in range(len(df2))]
     # rename in column
     player_df.rename(columns = {'id':'player_id'}, inplace = True)
     return player_df
@@ -99,15 +124,23 @@ def get_team_info(season_id):
 
 # get team information
 def get_draft_strategy(season_id, league_type = 'STANDARD'):
-    url = "https://fantasy.espn.com/apis/v3/games/flb/seasons/{}/segments/0/leagues/55646?view=kona_player_info_edit_draft_strategy".format(2022)
+    url = "https://fantasy.espn.com/apis/v3/games/flb/seasons/{}/segments/0/leagues/55646?view=kona_player_info_edit_draft_strategy".format(season_id)
     r = requests.get(url,
                      headers=custom_headers_2,
                      cookies=espn_cookies)
     draft_strat_data = r.json()
-    ids = []
-    rankings = []
-    for i in range(0, 3000):
-        ids = ids + [draft_strat_data['players'][i]['player']['id']]
-        rankings = rankings + [draft_strat_data['players'][i]['player']['draftRanksByRankType']['STANDARD']['rank']]
-    df = pd.DataFrame({'id': ids, 'ranking':rankings})
+    if season_id < 2022:
+        ids = []
+        rankings = []
+        for i in range(0, 3000):
+                ids = ids + [draft_strat_data['players'][i]['player']['id']]
+                rankings = rankings + [i + 1]
+        df = pd.DataFrame({'id': ids, 'pre_draft_ranking':rankings})
+    else:
+        ids = []
+        rankings = []
+        for i in range(0, 3000):
+            ids = ids + [draft_strat_data['players'][i]['player']['id']]
+            rankings = rankings + [draft_strat_data['players'][i]['player']['draftRanksByRankType']['STANDARD']['rank']]
+        df = pd.DataFrame({'id': ids, 'pre_draft_ranking':rankings})
     return df
